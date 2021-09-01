6021d43a29605f677a2264c71ba947d614d7516e
Junha <mythpow@gmail.com>
2021-09-01 17:32:17 +0900

    readme

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -13,6 +13,7 @@ body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     text-size-adjust: none;
+    background-color: #FAFAFA;
 }
 
 html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, dfn, del, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video {
